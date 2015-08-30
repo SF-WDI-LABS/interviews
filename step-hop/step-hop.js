43a5d145d846fa37750e5a1b24d3452d244d7d42
bgveenstra <b.g.veenstra@gmail.com>
2015-08-30 16:12:41 -0700

    added 3-step hopping solution

--- step-hop/step-hop.js
+++ step-hop/step-hop.js
@@ -8,18 +8,18 @@ var waysToHop = function(stairs){
 	}
 }
 
-for (var i=0; i<5; i++){
-	console.log(waysToHop(i)+" ways to hop "+i+" stairs");
-}
+// for (var i=0; i<5; i++){
+// 	console.log(waysToHop(i)+" ways to hop "+i+" stairs");
+// }
 
 // WAY MORE EFFICIENT DYNAMIC PROGRAMMING
 // each number of stairs is calculated only once
 var waysToHopDP = function(stairs){
 	// "memoize" (store) previous values
 	// so we don't have to calculate them over and over!
-	var memo = {0:1, 1:1}; 
-	for (var i=2; i<=stairs; i++){
-		memo[i] = memo[i-1] + memo[i-2];
+	var memo = {0:1}; 
+	for (var i=1; i<=stairs; i++){
+		memo[i] = memo[i-1] + (memo[i-2] || 0);
 	}
 	return memo[stairs];
 }
@@ -28,3 +28,16 @@ for (var i=0; i<5; i++){
 	console.log(waysToHopDP(i)+" ways to hop "+i+" stairs");
 }
 
+var waysToHop3DP = function(stairs){
+	var memo = {0:1};
+	for (var i=0; i<=stairs; i++){
+		if (!memo[i]){
+			memo[i] = memo[i-1] + (memo[i-2] || 0) + (memo[i-3] || 0);
+		}
+	}
+	return memo[stairs];
+}
+
+for (var i=0; i<5; i++){
+	console.log(waysToHop3DP(i)+" ways to hop "+i+" stairs");
+}
