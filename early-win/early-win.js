a18a0843fcaf0f0aa2bab47cb1e53b6a7c9b160c
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2015-09-19 18:49:01 -0700

    cleanup

--- early-win/early-win.js
+++ early-win/early-win.js
@@ -23,15 +23,13 @@ CYOATree.prototype.findEarlyWin = function(){
 	}
 	// we've gone through the entire story tree, and there are no happy endings
 	// interviewees should ask what to do here
-	// we'll adopt a js-array-like convention of returning -1
+	// we'll adopt a convention of returning -1
 	return -1;
 }
 
 var CYOANode = function (storySection, choices){
-	this.data = storySection; // the section of the story starting at this page
-	// "Once upon a time... "
-	this.children = choices;	// the choices of pages/CYOANodes that we can go to from here
-	// {43: CYOANode@2987293423, 89: CYOANode@298432923842, 200: CYOANode@203492390432}
+	this.data = storySection; 	// a string, the section of the story starting at this page
+	this.children = choices;	// an array, holds the CYOANodes that we can go to directly from here
 }
 
 CYOANode.prototype.isHappyEnding = function(){
@@ -41,31 +39,25 @@ CYOANode.prototype.isHappyEnding = function(){
 }
 
 
-// ////////////////Testing
+//////////////////Testing
 
 CYOANode.prototype.addChoice = function(choiceNode){
 	this.children.push(choiceNode);
 }
 
-// var pg67 = new CYOANode("You open the door, and find the Sword of Truth! Congratulations!", []);
-// var pg13 = new CYOANode("As you lift the jar to bring her with you, the fairy spins out, and you spin in. You are trapped in the jar. YOU HAVE LOST.", [])
-// var pg43 = new CYOANode("The fairy pouts, but then she waves her hand, and a golden door appears in the tree next to you.", [pg67, pg13]);
-// var pg89 = new CYOANode("*You* are now trapped inside the jar! The fairy blows you a kiss as she flies away.  YOU HAVE LOST.", []);
-// var pg1 = new CYOANode("Once upon a time...", [pg43, pg89]);
-
-var pg67 = new CYOANode("pg67 Congratulations!", []);
-var pg13 = new CYOANode("pg13", [])
-var pg43 = new CYOANode("pg43", [pg67, pg13]);
-var pg89 = new CYOANode("pg89", []);
-var pg1 = new CYOANode("pg1", [pg43, pg89]);
+var pg67 = new CYOANode("pg67 text -- Congratulations!", []);
+var pg13 = new CYOANode("pg13 text -- You have died.", [])
+var pg43 = new CYOANode("pg43 text", [pg67, pg13]);
+var pg89 = new CYOANode("pg89 text -- You have died.", []);
+var pg1 = new CYOANode("pg1 text -- Once upon a time...", [pg43, pg89]);
 var book = new CYOATree(pg1);
-// 		89L
+// 	   89L
 // 	/ 
-// 1 			67W
-// 	\		/
-// 		43 
-// 			\
-// 				13L
+// 1 		  67W
+// 	\	/
+// 	   43 
+// 		\
+// 	  	  13L
 
 console.log(book.findEarlyWin())  //2
 
